d4568fe3cb627029057e041c23aaff0ca76ab334
Matteus Varlesse <Varlesse04@gmail.com>
2024-04-17 16:16:44 -0300

    feat: no change drawn_number

--- lib/guessing_game.ex
+++ lib/guessing_game.ex
@@ -1,18 +1,25 @@
 defmodule Games.GuessingGame do
-  def play(count \\ 0) do
-    inputValue =
+  def play(count \\ 0, random_value \\ 0) do
+    input_value =
       IO.gets("Guess a number between 1 and 10: ") |> String.trim() |> String.to_integer()
 
-    random_value = Enum.random(1..10)
+    if(random_value == 0) do
+      win?(input_value, random_value, count)
+    else
+      random_value = Enum.random(1..10)
+      win?(input_value, random_value, count)
+    end
+  end
 
-    if(inputValue === random_value) do
+  defp win?(input_value, random_value, count) do
+    if(input_value === random_value) do
       IO.puts("You Win")
     else
       if(count >= 4) do
         IO.puts("You lose! the answer was 4")
       else
         IO.puts("Incorrect!")
-        play(count + 1)
+        play(count + 1, random_value)
       end
     end
   end
